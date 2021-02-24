c9e89947bc908b2dfbf101a6ed40c2d03d430d40
sznekk <albino01998@gmail.com>
2021-02-24 19:12:37 +0100

    runnable database fix

--- database.py
+++ database.py
@@ -6,16 +6,16 @@ import sqlite3
 
 conn = sqlite3.connect('datagov.db')
 c = conn.cursor()
-c.execute('''CREATE TABLE stocks
-             (date text, trans text, symbol text, qty real, price real)''')
-c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
+#c.execute('''CREATE TABLE stocks
+#             (date text, trans text, symbol text, qty real, price real)''')
+#c.execute("INSERT INTO stocks VALUES ('2006-01-05','BUY','RHAT',100,35.14)")
 
 # Save (commit) the changes
-conn.commit()
+#conn.commit()
 
 # We can also close the connection if we are done with it.
 # Just be sure any changes have been committed or they will be lost.
-conn.close()
+
 
 for row in c.execute('SELECT * FROM stocks ORDER BY price'):
         print(row)
@@ -24,3 +24,4 @@ for row in c.execute('SELECT * FROM stocks ORDER BY price'):
 ('2006-03-28', 'BUY', 'IBM', 1000, 45.0)
 ('2006-04-06', 'SELL', 'IBM', 500, 53.0)
 ('2006-04-05', 'BUY', 'MSFT', 1000, 72.0)
+conn.close()
